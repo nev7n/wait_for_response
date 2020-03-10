75f9280425f3e9122d068835f5cda87d38d874cc
Justin Knight <justin@glassraven.com>
2020-03-10 16:33:50 +0000

    Fix flags not being parsed

--- main/main.go
+++ main/main.go
@@ -13,7 +13,9 @@ func main() {
 	var responseCode = flag.Int("code", 200, "Response code to wait for")
 	var timeout = flag.Int("timeout", 2000, "Timeout before giving up in ms")
 	var interval = flag.Int("interval", 200, "Interval between polling in ms")
+	flag.Parse()
 
+	fmt.Printf("Polling URL `%s` for response code %d for up to %d ms at %d ms intervals\n", *url, *responseCode, *timeout, *interval)
 	startTime := time.Now()
 	timeoutDuration := time.Duration(*timeout) * time.Millisecond
 	sleepDuration := time.Duration(*interval) * time.Millisecond
@@ -26,7 +28,7 @@ func main() {
 		time.Sleep(sleepDuration)
 		elapsed := time.Now().Sub(startTime)
 		if elapsed > timeoutDuration {
-			fmt.Printf("Timed out")
+			fmt.Printf("Timed out\n")
 			os.Exit(1)
 		}
 	}
