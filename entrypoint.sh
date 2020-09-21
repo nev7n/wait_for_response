3f8623499b514ae62eefd60b796cc16b939e76c8
Shane Krueger <shane@acdmail.com>
2020-09-21 10:07:01 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 hostip=$(ip route show | awk '/default/ {print $3}')
-/app/wait_for_response "-url=$1" "-code=$2" "-timeout=$3" "-interval=$4" "-localhost=${hostip}"
+/app/wait_for_response "-url=$1" "-code=$2" "-timeout=$3" "-interval=$4" "-localhost=${hostip}" "-sleep=$5"
