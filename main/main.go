eab1fa40c95faba344a1b04863da47d768b5eea2
Till Klampaeckel <till@php.net>
2026-04-01 15:18:42 +0200

    feat: basic auth
    this commit adds support for basic authentication in case the target
    is protected by that.
    
    picked the idea from one of the forks of this action, but different
    approach, but giving credit.
    
    Co-Authored-By: brendan-hall <brendan.j.hall@bath.edu>

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"net/http"
@@ -16,29 +17,52 @@ var (
 
 func main() {
 	var (
+		// command flags
 		url          = flag.String("url", "http://localhost/", "URL to poll")
 		responseCode = flag.Int("code", 200, "Response code to wait for")
 		timeout      = flag.Int("timeout", 30000, "Timeout before giving up in ms")
 		interval     = flag.Int("interval", 1000, "Interval between polling in ms")
 		localhost    = flag.String("localhost", "", "Ip address to use for localhost")
+		username     = flag.String("username", "", "HTTP Basic Auth: username")
+		password     = flag.String("password", "", "HTTP Basic Auth: password")
 	)
 
 	flag.Parse()
 
+	var (
+		// runtime
+		userAgent = fmt.Sprintf(
+			"nev7n/wait_for_response/%s (+https://github.com/nev7n/wait_for_response)",
+			version)
+
+		startTime        = time.Now()
+		timeoutDuration  = time.Duration(*timeout) * time.Millisecond
+		intervalDuration = time.Duration(*interval) * time.Millisecond
+		basicAuth        = ""
+	)
+
 	fmt.Printf("Polling URL `%s` for response code %d for up to %d ms at %d ms intervals\n",
 		*url, *responseCode, *timeout, *interval)
-
-	startTime := time.Now()
-	timeoutDuration := time.Duration(*timeout) * time.Millisecond
-	intervalDuration := time.Duration(*interval) * time.Millisecond
 
 	if *localhost != "" && strings.Contains(*url, "localhost") {
 		*url = strings.ReplaceAll(*url, "localhost", *localhost)
 	}
 
-	userAgent := fmt.Sprintf(
-		"nev7n/wait_for_response/%s (+https://github.com/nev7n/wait_for_response)",
-		version)
+	// validate both are set
+	if username != nil || password != nil {
+		if username == nil {
+			fmt.Printf("Missing username, password was set")
+			os.Exit(1)
+		}
+
+		if password == nil {
+			fmt.Printf("Missing password, username was set (%s)", *username)
+			os.Exit(1)
+		}
+
+		basicAuth = base64.StdEncoding.EncodeToString(
+			fmt.Appendf(nil, "%s:%s", *username, *password))
+	}
 
 	for {
 		ctx, cancel := context.WithTimeout(context.Background(), intervalDuration)
@@ -49,6 +73,10 @@ func main() {
 			continue
 		}
 		req.Header.Set("User-Agent", userAgent)
+
+		if basicAuth != "" {
+			req.Header.Set("Authorization", "Basic "+basicAuth)
+		}
 
 		client := http.Client{}
 		res, err := client.Do(req)
